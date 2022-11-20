20c466c57b4f7abf49f9ee46b0b0397a774159d6
Charise Walraven <cherri.walraven@gmail.com>
2022-11-20 15:29:59 +0100

    writes basic unit test for beer API

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,7 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:real_brew/services/beer_api.dart';
 
 void main() {
-  test('Unit test test', () {
-    expect(1, 1);
+  group('Services', () {
+    group('BeerAPI', () {
+      final BeerAPI beerAPI = BeerAPI();
+      group('.getBeers', () {
+        var getBeersResult = beerAPI.getBeers();
+        test('Should return a List', () {
+          expect(getBeersResult, isList);
+          // expect(getBeersResult.length, equals(25));
+        });
+        test('With 25 items', () {
+          expect(getBeersResult.length, equals(25));
+        });
+      });
+    });
   });
 }
