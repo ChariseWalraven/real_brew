d364135ca710eae2614b99c54e78c754d4304f71
Charise Walraven <cherri.walraven@gmail.com>
2022-11-20 21:27:27 +0100

    removes unused import

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:real_brew/models/amount.dart';
