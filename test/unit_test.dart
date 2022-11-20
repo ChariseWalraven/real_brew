56cfb477a9288c7e222cc3d64465e96447b5872c
Charise Walraven <cherri.walraven@gmail.com>
2022-11-20 16:11:47 +0100

    swaps mock class with real class to actually test it

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,21 +1,29 @@
+// ignore_for_file: dead_code
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
 import 'package:real_brew/services/beer_api.mocks.dart';
-// import 'package:real_brew/services/beer_api.dart';
+import 'package:real_brew/services/beer_api.dart';
 
 void main() {
+  const bool mockBeerAPI = false;
   group('Services', () {
     group('BeerAPI', () {
-      final MockBeerAPI beerAPI = MockBeerAPI();
+      BeerAPI beerAPI = BeerAPI();
+      if (mockBeerAPI) {
+        beerAPI = MockBeerAPI();
+      }
       group('.ping', () {
         test('Should return -1', () {
-          when(beerAPI.ping()).thenReturn(-1);
+          if (mockBeerAPI) when(beerAPI.ping()).thenReturn(-1);
           expect(beerAPI.ping(), -1);
         });
       });
       group('.getBeers', () {
-        when(beerAPI.getBeers()).thenReturn(List.filled(25, ''));
+        if (mockBeerAPI) {
+          when(beerAPI.getBeers()).thenReturn(List.filled(25, ''));
+        }
         var getBeersResult = beerAPI.getBeers();
         test('Should return a List', () {
           expect(getBeersResult, isList);
