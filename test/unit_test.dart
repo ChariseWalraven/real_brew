19ca65a4f321b882866a5dd4b95122c165d9d819
Charise Walraven <cherri.walraven@gmail.com>
2022-11-20 16:41:51 +0100

    refactors for readability & test settings file

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,31 +1,35 @@
-// ignore_for_file: dead_code
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
 import 'package:real_brew/services/beer_api.mocks.dart';
 import 'package:real_brew/services/beer_api.dart';
 
+import 'test.settings.dart';
+
 void main() {
-  const bool mockBeerAPI = false;
   group('Services', () {
     group('BeerAPI', () {
       BeerAPI beerAPI = BeerAPI();
-      if (mockBeerAPI) {
+
+      // setup mocks if using
+      if (kMockBeerAPI) {
         beerAPI = MockBeerAPI();
+
+        when(beerAPI.ping()).thenReturn(-1);
+        when(beerAPI.getBeers()).thenReturn(
+          Future.delayed(
+            const Duration(milliseconds: 5),
+            () => List.filled(25, ''),
+          ),
+        );
       }
+
       group('.ping', () {
         test('Should return -1', () {
-          if (mockBeerAPI) when(beerAPI.ping()).thenReturn(-1);
-
           expect(beerAPI.ping(), -1);
         });
       });
       group('.getBeers', () {
-        if (mockBeerAPI) {
-          when(beerAPI.getBeers()).thenReturn(Future.delayed(
-              const Duration(milliseconds: 5), () => List.filled(25, '')));
-        }
         test('Should return a List', () async {
           expect(await beerAPI.getBeers(), isList);
         });
