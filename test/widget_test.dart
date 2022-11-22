0bf3b6248af97a29202db79ddecb7a81287d76de
Charise Walraven <cherri.walraven@gmail.com>
2022-11-22 16:30:49 +0100

    gets basic home screen widget test to pass

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,14 +11,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:real_brew/main.dart';
 
 void main() {
-  // TODO: widget tests
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  group('Home Screen', () {
+    testWidgets('Has a Text with "Choose your beer", and a BeersList', (WidgetTester tester) async {
+      await tester.pumpWidget(const MaterialApp(home: HomeScreen()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      expect(find.byType(Text), findsOneWidget);
+      expect(find.text('Choose your beer'), findsOneWidget);
+      expect(find.byType(BeersList), findsOneWidget);
+    });
 
     // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
