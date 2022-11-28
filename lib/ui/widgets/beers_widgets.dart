c59de55a54d1a5a2b1e3ffd77c5377f9575a3023
Charise Walraven <cherri.walraven@gmail.com>
2022-11-28 12:40:27 +0100

    adds basic beer item widget

--- lib/ui/widgets/beers_widgets.dart
+++ lib/ui/widgets/beers_widgets.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:real_brew/models/beer_recipe.dart';
 import 'package:real_brew/state/beers.dart';
+import 'package:real_brew/util/functions.dart';
 
 class BeersList extends ConsumerWidget {
   const BeersList({super.key});
@@ -25,10 +26,43 @@ class BeersList extends ConsumerWidget {
           shrinkWrap: true,
           itemBuilder: (context, index) {
             BeerRecipe beer = beers[index];
-            return Text(beer.name);
+            return BeersListItem(beer: beer);
           },
         );
       },
+    );
+  }
+}
+
+class BeersListItem extends StatelessWidget {
+  const BeersListItem({
+    Key? key,
+    required this.beer,
+  }) : super(key: key);
+
+  final BeerRecipe beer;
+
+  @override
+  Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
+    var theme = Theme.of(context);
+    return SizedBox(
+      child: Container(
+        height: screenSize.height / 2.5,
+        width: screenSize.width / 2,
+        decoration: BoxDecoration(
+            image: DecorationImage(image: NetworkImage(beer.imageUrl))),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(style: theme.textTheme.bodyMedium, beer.name),
+            const IconButton(
+              onPressed: unimplimentedOnPress,
+              icon: Icon(Icons.favorite_border),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
