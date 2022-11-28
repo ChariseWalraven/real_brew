2349912ac63cdf2d809760fc8035c805a73e4700
Charise Walraven <cherri.walraven@gmail.com>
2022-11-28 16:17:05 +0100

    uses scaffold widget

--- lib/ui/screens/detail_screen.dart
+++ lib/ui/screens/detail_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:real_brew/ui/widgets/real_brew_scaffold.dart';
 
 class DetailScreen extends StatelessWidget {
   const DetailScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("This is the detail screen");
+    return const RealBrewScaffold(
+      body: Text("This is the detail screen"),
+    );
   }
 }
