d3e55464c4daec5c45206cc62640912164c9d309
Charise Walraven <cherri.walraven@gmail.com>
2022-11-30 13:55:54 +0100

    adds some padding

--- lib/ui/screens/detail/method.dart
+++ lib/ui/screens/detail/method.dart
@@ -14,49 +14,56 @@ class Method extends StatelessWidget {
     // use the first mash temp because most recipes only have one. Potential for having a list later.
     final m.MashTemp firstMashTemp = method.mashTemp.first;
     final m.Fermentation fermentation = method.fermentation;
-    return Column(
-      children: [
-        const SectionTitle('METHOD'),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const BoldText(
-              'Mash Temp:',
-              fontWeight: FontWeight.w700,
-            ),
-            Text(
-                '${firstMashTemp.temp.value} ${firstMashTemp.temp.unit} | ${firstMashTemp.duration} min')
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            const BoldText(
-              'Fermentation:',
-              fontWeight: FontWeight.w700,
-            ),
-            Text('${fermentation.temp.value}° ${fermentation.temp.unit}')
-          ],
-        ),
-        if (method.twist != null && method.twist!.isNotEmpty)
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: Wrap(
-              runSpacing: 5,
-              children: [
-                const BoldText(
-                  'Twist',
-                  fontWeight: FontWeight.w700,
-                ),
-                Text(
-                  method.twist!,
-                  overflow: TextOverflow.visible,
-                  textAlign: TextAlign.justify,
-                ),
-              ],
-            ),
-          )
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: SectionTitle('METHOD'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const BoldText(
+                'Mash Temp:',
+                fontWeight: FontWeight.w700,
+              ),
+              Text(
+                  '${firstMashTemp.temp.value} ${firstMashTemp.temp.unit} | ${firstMashTemp.duration} min')
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const BoldText(
+                'Fermentation:',
+                fontWeight: FontWeight.w700,
+              ),
+              Text('${fermentation.temp.value}° ${fermentation.temp.unit}')
+            ],
+          ),
+          if (method.twist != null && method.twist!.isNotEmpty)
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Wrap(
+                runSpacing: 5,
+                children: [
+                  const BoldText(
+                    'Twist',
+                    fontWeight: FontWeight.w700,
+                  ),
+                  Text(
+                    method.twist!,
+                    overflow: TextOverflow.visible,
+                    textAlign: TextAlign.justify,
+                  ),
+                ],
+              ),
+            )
+        ],
+      ),
     );
   }
 }
